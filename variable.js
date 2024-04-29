df5c99a36256ecd99e4f57e437411ee766fc6273
masdefry <muhammaddefryan@gmail.com>
2024-04-29 20:19:03 +0700

    Intro Variable

---
+++ variable.js
@@ -0,0 +1,58 @@
+console.log('Hello, World!')
+console.log(123)
+
+
+
+// VARIABLE: Tempat untuk Menyimpan Data
+
+// Case. Client Ingin Menampilkan Kata "Hello, Purwadhika!" Sebanyak 5x
+console.log('Hello, Purwadhika!')
+console.log('Hello, Purwadhika!')
+console.log('Hello, Purwadhika!')
+console.log('Hello, Purwadhika!')
+console.log('Hello, Purwadhika!')
+
+
+
+// Declare Variable
+// var variableName
+var greeting // Data Kosong
+// Assign Variable
+greeting = 'Hello, Purwadhika!'
+console.log(greeting)
+console.log(greeting)
+console.log(greeting)
+console.log(greeting)
+console.log(greeting)
+
+// Declare + Assign Variable
+var greeting1 = 'Hi, JCWD!'
+console.log(greeting1)
+
+
+
+// Rules:
+// - Variable Name Harus Jelas
+var stadiumName = 'Soekarno'
+// - Diawali dengan Huruf, _, $
+var $tadiumName
+var _stadiumName
+// - Format PascalCase, snake_case, camelCase
+var CampusPwd
+var campus_pwd
+var campusPwd
+var indonesianStadiumName
+// - Case Sensitive
+var studentName = 'Defryan'
+console.log(studentName)
+// - Menggunakan Syntax JS
+// var console.log 
+// var function
+
+
+
+var point = 100
+point = 10
+
+var discount = 10
+var discount = 100
