d6ddf089165fe13cca4056609beb43aa2496ef1e
masdefry <muhammaddefryan@gmail.com>
2024-05-02 18:50:47 +0700

    Comparison Operator

---
+++ comparisonoperator.js
@@ -0,0 +1,9 @@
+// COMPARISON OPERATOR: Hasil dari Perbandingan yaitu Boolean
+// >, >=, <, <=, ==, ===, !=, !== 
+
+console.log(5 > 5) // false
+console.log(5 >= 5) // true
+console.log(10 < 20) // true
+console.log(10 <= 100) // true
+console.log(10 == '10') // true
+console.log(10 === '10') // false
