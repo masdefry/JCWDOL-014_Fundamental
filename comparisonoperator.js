a42fe16650d824867f4ba117872e78098fdd2328
masdefry <muhammaddefryan@gmail.com>
2024-05-05 12:43:46 +0700

    Day03 Done

--- comparisonoperator.js
+++
@@ -1,9 +0,0 @@
-// COMPARISON OPERATOR: Hasil dari Perbandingan yaitu Boolean
-// >, >=, <, <=, ==, ===, !=, !== 
-
-console.log(5 > 5) // false
-console.log(5 >= 5) // true
-console.log(10 < 20) // true
-console.log(10 <= 100) // true
-console.log(10 == '10') // true
-console.log(10 === '10') // false
---
+++ loop.js
@@ -0,0 +1,27 @@
+// LOOPING
+
+while(condition){
+    action 
+}
+
+do{
+    action 
+}while(condition)
+
+
+
+let start = 1
+while(start <= 10){ // TRUE; TRUE; TRUE;
+    console.log(start)
+    start++
+}
+
+
+
+
+
+let startCount = 1 // 1 ---> 11
+do{
+    console.log(startCount) // 1
+    startCount+=10
+}while(startCount<3) // FALSE
