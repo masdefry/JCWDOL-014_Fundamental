021bc4952a6551fb5d91702f0f24109641fd37c8
masdefry <muhammaddefryan@gmail.com>
2024-05-07 21:11:42 +0700

    Review Day05 Slide02

--- Exercise/Day05.js
+++ Exercise/Day05.js
@@ -45,3 +45,63 @@ function FilteringNewElement(arr, newElement){
 }
 
 console.log(FilteringNewElement([1, 5, 10, 15], 3))
+
+
+
+// SLIDE-02
+// 01
+function SliceArrayBasedOnMaxSize(maxSize, ...numbers){
+    return numbers.slice(0, maxSize)
+}
+
+console.log(SliceArrayBasedOnMaxSize(5, 10, 3, 7, 100, 25, 55))
+
+// 02
+function CombineTwoArray(arr1, arr2){
+    return [...arr1, ...arr2]
+}
+
+console.log(CombineTwoArray([1, 2, 3], [4, 5, 6]))
+
+// 03
+function FindDuplicate(arrNumbers){
+    const arrResult = []
+    arrNumbers.forEach(number => {
+        if(arrNumbers.indexOf(number) !== arrNumbers.lastIndexOf(number)){
+            if(!arrResult.includes(number)){
+                arrResult.push(number)
+            }
+        } 
+    })
+
+    return arrResult
+}
+
+console.log(FindDuplicate([1, 2, 2, 3, 3]))
+
+
+
+// 04
+function FindDifferenceElementInTwoArray(arr1, arr2){
+    const differentElement = []
+
+    arr1.forEach(number => {
+        if(!arr2.includes(number)){
+            if(!differentElement.includes(number)){
+                differentElement.push(number)
+            }
+        }
+    })
+
+    arr2.forEach(number => {
+        if(!arr1.includes(number)){
+            if(!differentElement.includes(number)){
+                differentElement.push(number)
+            }
+        }
+    })
+
+    return differentElement
+}
+
+console.log(FindDifferenceElementInTwoArray([1, 3, 5], [2, 4, 5]))
