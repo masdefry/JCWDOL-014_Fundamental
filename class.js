edaac0d46bab0d83ce7c203c48d066c533d76c76
masdefry <muhammaddefryan@gmail.com>
2024-05-12 13:41:56 +0700

    Inheritance

--- class.js
+++ class.js
@@ -12,29 +12,78 @@
 //     console.log(`${item.name} = ${item.price}`)
 // })
 
-class Products{
-    name = '' 
-    price = 0 
-    discount = 0
+// class Products{
+//     name = '' 
+//     price = 0 
+//     discount = 0
 
-    constructor(name, price, discount){
-        this.name = name; 
+//     constructor(name, price, discount){
+//         this.name = name; 
+//         this.price = price;
+//         this.discount = discount; 
+//     }
+// }
+
+// const product1 = new Products('Apel', 10000, 10)
+// console.log(product1)
+
+// const arrProducts = [
+//     new Products('Apel', 10000, 10), 
+//     new Products('Jeruk', 15000, 15), 
+//     new Products('Anggur', 20000, 20)
+// ]
+
+// Case.
+// Di sebuah supermarket, terdapat beberapa product category. Mulai dari electronic, fashion dan snack. 
+// Setiap product category memiliki identitas yang berbeda-beda, seperti product name, product stock, dll.
+// 1. Jabarkan apa saja identitas yg dimiliki untuk masing-masing product category!
+// 2. Class berdasarkan product category
+
+// Electronic: Product Name, Product Price, Product Discount, Product Stock, Product SerialNumber, Product Guarantee, Product Color 
+// Fashion: Product Name, Product Price, Product Discount, Product Stock, Product Size, Product Color, Product Material
+// Snack: Product Name, Product Price, Product Discount Product Stock, Product Flavour, Product Expiry, Product Netto
+
+// INHERITANCE: Pewarisan Property
+class Products{
+    constructor(name, price, discount, stock, color){
+        this.name = name;
         this.price = price;
-        this.discount = discount; 
+        this.discount = discount;
+        this.stock = stock;
+        this.color = color;
     }
 }
 
-const product1 = new Products('Apel', 10000, 10)
-console.log(product1)
+class ProductElectronic extends Products{
+    constructor(serialNumber, guarantee, color, name, price, discount, stock){
+    super(name, price, discount, stock, color)
+        this.serialNumber = serialNumber;
+        this.guarantee = guarantee;
+    }
+}
 
-const arrProducts = [
-    new Products('Apel', 10000, 10), 
-    new Products('Jeruk', 15000, 15), 
-    new Products('Anggur', 20000, 20)
-]
+const inputElectronic = new ProductElectronic('123', '2024', 'Blue', 'A')
+console.log(inputElectronic)
 
-console.log(arrProducts)
+class ProductFashion{
+    constructor(name, price, discount, stock, size, color, material){
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.stock = stock;
+        this.size = size; 
+        this.material = material;
+    }
+}
 
-
-
-// Case. Sebuah Class untuk Data Student yang ada di Pwd
+class ProductSnack{
+    constructor(name, price, discount, stock, flavour, expiry, netto){
+        this.name = name;
+        this.price = price;
+        this.discount = discount;
+        this.stock = stock;
+        this.flavour = flavour;
+        this.expiry = expiry; 
+        this.netto = netto;
+    }
+}
