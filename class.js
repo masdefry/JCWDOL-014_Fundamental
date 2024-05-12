1779eba38aa047b60891e1e197769949814102d7
masdefry <muhammaddefryan@gmail.com>
2024-05-12 14:17:24 +0700

    Encapsulation, Getter dan Setter

--- class.js
+++ class.js
@@ -87,3 +87,36 @@ class ProductSnack{
         this.netto = netto;
     }
 }
+
+
+
+// ENCAPSULATION: Membungkus Data kedalam 1 Unit yang bernama Class
+// Public Property
+// Private Property
+
+class Users{
+    username; 
+    email;
+    #password;
+    constructor(username, password){
+        this.username = username;
+        this.#password = password;
+    }
+
+    get showPassword(){
+        console.log(this.#password)
+    }
+
+    set validateEmail(email){
+        if(!email.includes('@')){
+            console.log('Email Not Valid!')
+        }else{
+            this.email = email
+        }
+    }
+}
+
+const userAccount1 = new Users('ryandefryan', 'abc123')
+userAccount1.validateEmail = 'ryan@gmail.com'
+console.log(userAccount1)
+// console.log(userAccount1.showPassword())
---
+++ closurefunction.js
@@ -0,0 +1,10 @@
+function A(){
+    
+    const greeting = 'Hello';
+    function B(){
+        console.log(greeting)
+    }
+    B();
+}
+
+A()
